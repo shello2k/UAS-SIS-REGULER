bcdb1ad06729b3a41a4dd3ff025b4cf1f29c1681
FasaBaskara <baskara2201@gmail.com>
2025-01-02 04:47:49 +0700

    Merubah route untuk ke setiap role

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart'; // Untuk kReleaseMode
 import 'package:device_preview/device_preview.dart';
-import 'package:flutter_application_1/pages/admin_dashboard.dart';
-import 'package:flutter_application_1/pages/adminn_dashboard.dart';
-import 'package:flutter_application_1/pages/list_of_request.dart';
-import 'package:flutter_application_1/pages/new_request.dart';
-import 'package:flutter_application_1/pages/notification_page.dart';
-import 'package:flutter_application_1/pages/student_dashboard.dart';
+import 'package:flutter_application_1/pages/faculty_dashboard.dart';
+import 'package:flutter_application_1/pages/head_dashboard.dart';
 import 'package:provider/provider.dart';
 import 'app_state.dart';
 import 'login_page.dart';
 
-void main() => runApp(
-      DevicePreview(
-        enabled: !kReleaseMode,
-        builder: (context) => ChangeNotifierProvider(
-          create: (_) => AppState(),
-          child: MyApp(),
-        ),
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'pages/adminn_dashboard.dart';
+import 'pages/student_dashboard.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  ).catchError((error) {
+    print('Error initializing Firebase: $error');
+  });
+
+  runApp(
+    DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => ChangeNotifierProvider(
+        create: (_) => AppState(),
+        child: MyApp(),
       ),
-    );
+    ),
+  );
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      useInheritedMediaQuery: true, // Integrasi dengan DevicePreview
+      useInheritedMediaQuery: true,
       locale: DevicePreview.locale(context),
       builder: DevicePreview.appBuilder,
       theme: ThemeData.light(),
       darkTheme: ThemeData.dark(),
       themeMode: ThemeMode.system,
-      home: StudentDashboard(), // Halaman utama Anda
+      home: LoginPage(),
+      routes: {
+        'login': (context) => LoginPage(),
+        'admin_dashboard': (context) => AdminDashboard(),
+        'student_dashboard': (context) => StudentDashboard(),
+        'head_dashboard': (context) => HeadDashboard(),
+        'faculty_dashboard': (context) => FacultyDashboard(),
+      },
     );
   }
 }
